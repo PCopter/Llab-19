00446156139904eb8628fa7298bf9b1998382451
Copter <copter.phanuwat@gamil.com>
2022-01-25 23:44:23 +0700

    Llab 19 0k

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -3,8 +3,9 @@
 #include<vector>
 #include<string>
 #include<cstdlib>
-
 using namespace std;
+
+
 
 char score2grade(int score){
     if(score >= 80) return 'A';
@@ -20,19 +21,61 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string filename,vector<string> &names, vector<int> &scores,vector<char> &grades){
+    ifstream sources(filename);
+    string textline;
+    char format[] ="%[^:]: %d %d %d";
+    
+    while(getline(sources,textline))
+    {
+        char name[100];
+        int score1,score2,score3;
+        sscanf(textline.c_str(),format,name,&score1,&score2,&score3);
+        names.push_back(name);
+        scores.push_back(score1+score2+score3);
+        grades.push_back(score2grade(score1+score2+score3));
+    }
 
 }
 
-void getCommand(){
+void getCommand(string &command,string &key){
+    string cmdline;
+    char cmdch[100];
+    char keych[100];
+    char format[]="%s %[^\n] ";
+    cout << "Please input your command: ";
+    getline(cin,cmdline);
+    sscanf(cmdline.c_str(),format,cmdch,keych);
+    command = cmdch;
+    key = keych;
 
 }
 
-void searchName(){
-
+void searchName(vector<string> names, vector<int> scores, vector<char> grades, string key)
+{
+cout << "---------------------------------\n";
+int result = -1;
+for(unsigned int i= 0;i<names.size();i++)
+{
+    if(toUpperStr(names.at(i))==key)result =i;
+}
+if(result != -1)
+{
+    cout << names.at(result) << "'s score = " << scores.at(result) << "\n";
+    cout << names.at(result) << "'s grade = " << grades.at(result) << "\n";
+}
+    else cout << "Cannot found." << "\n";
+    cout << "---------------------------------\n";
 }
 
-void searchGrade(){
+void searchGrade(vector<string> names, vector<int> scores, vector<char> grades, string key){
+    cout << "---------------------------------\n";
+    for(unsigned int i= 0;i<names.size();i++){
+        if(grades.at(i)==*key.c_str()){
+            cout << names.at(i) << " (" << scores.at(i) << ") " << "\n";
+        }
+    }
+cout << "---------------------------------\n";
 
 }
 
